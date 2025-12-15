0c419a8934dfeec8269d585b3ff3f3026263fb14
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2025-12-15 01:01:26 +0000

    add `Home` page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>Em Construção</h1>
+      <p>
+        Estamos preparando o Sistema Eletrônico de Informações para o
+        Licenciamento Ambiental para oferecer mais agilidade e transparência nos
+        processos de licenciamento. Em breve, novidades por aqui!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
