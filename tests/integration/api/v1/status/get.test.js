608019d1e6c8bd9439305c481003c9fcedd4be7a
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2025-12-31 00:47:43 +0000

    test: rename all descriptions to uso new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,18 +4,22 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  // Realiza uma requisição GET para o endpoint /api/v1/status
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  // Verifica se o status da resposta é 200
-  expect(response.status).toBe(200);
-  // Analisa o corpo da resposta como JSON
-  const responseBody = await response.json();
-  // Verifica se o conteúdo de updated_at é uma data ISO válida
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  // Verifica se a versão do banco de dados está correta
-  expect(responseBody.dependddecies.version).toEqual("16.11");
-  expect(responseBody.dependddecies.max_connections).toEqual(100);
-  expect(responseBody.dependddecies.opened_connections).toEqual(1);
+describe("GET /api/v1/status", () => {
+  describe("Annonymous user", () => {
+    test("Retrieving current system status", async () => {
+      // Realiza uma requisição GET para o endpoint /api/v1/status
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      // Verifica se o status da resposta é 200
+      expect(response.status).toBe(200);
+      // Analisa o corpo da resposta como JSON
+      const responseBody = await response.json();
+      // Verifica se o conteúdo de updated_at é uma data ISO válida
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      // Verifica se a versão do banco de dados está correta
+      expect(responseBody.dependddecies.version).toEqual("16.11");
+      expect(responseBody.dependddecies.max_connections).toEqual(100);
+      expect(responseBody.dependddecies.opened_connections).toEqual(1);
+    });
+  });
 });
